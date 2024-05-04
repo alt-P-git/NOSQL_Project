fa2101e1a3976bb21ec25138449fa60c3a83eefe
Prit <pritbhanushali012@gmail.com>
2024-05-05 03:04:25 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   app.js 	new file:   client/.eslintrc.cjs 	new file:   client/.gitignore 	new file:   client/index.html 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/postcss.config.js 	new file:   client/src/App.jsx 	new file:   client/src/assets/encryptShare logo.png 	new file:   client/src/assets/file.png 	new file:   client/src/assets/logo v2 nobg.png 	new file:   client/src/assets/logo v2.png 	new file:   client/src/assets/react.svg 	new file:   client/src/assets/sharing blue.svg 	new file:   client/src/components/AdvancedPasswordInput.jsx 	new file:   client/src/components/ApplicationTabs.jsx 	new file:   client/src/components/Banner.jsx 	new file:   client/src/components/CopyToClipboardBtn.jsx 	new file:   client/src/components/FilePreview.jsx 	new file:   client/src/components/Footer.jsx 	new file:   client/src/components/Header.jsx 	new file:   client/src/components/LandingFeatures.jsx 	new file:   client/src/components/PageHeader.jsx 	new file:   client/src/components/PricingTable.jsx 	new file:   client/src/components/ProgressBar.jsx 	new file:   client/src/components/SinglePricingElement.jsx 	new file:   client/src/components/UploadForm.jsx 	new file:   client/src/components/index.js 	new file:   client/src/index.css 	new file:   client/src/main.jsx 	new file:   client/src/pages/ApplicationLayout.jsx 	new file:   client/src/pages/ApplicationPage.jsx 	new file:   client/src/pages/FileDownload.jsx 	new file:   client/src/pages/FileHistory.jsx 	new file:   client/src/pages/HomeLayout.jsx 	new file:   client/src/pages/Landing.jsx 	new file:   client/src/pages/UserProfilePage.jsx 	new file:   client/src/pages/index.js 	new file:   client/tailwind.config.js 	new file:   client/vite.config.js 	new file:   controllers/main.js 	new file:   controllers/sendEmail.js 	new file:   db/connect.js 	new file:   models/File.js 	new file:   package-lock.json 	new file:   package.json 	new file:   routes/main.js

---
+++ controllers/sendEmail.js
@@ -0,0 +1,125 @@
+const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail')
+const Mailjet = require('node-mailjet');
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+
+
+
+/* const mailjet = Mailjet.apiConnect(
+    process.env.MJ_APIKEY_PUBLIC,
+    process.env.MJ_APIKEY_PRIVATE,
+    {
+      config: {},
+      options: {}
+    } 
+);
+
+const sendEmailEthereal = async (req, res) => {
+    let testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'jamar.hodkiewicz61@ethereal.email',
+            pass: 'vWjxnQ9QryVBm7GtJD'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Kuzma02" <aleksandarkuzmanovic02@gmail.com>',
+        to: 'jamar.hodkiewicz61@ethereal.email',
+        subject: 'Hello',
+        html: '<h2>Sending Emails With Node.js</h2>'
+    });
+
+    res.json(info);
+} */
+
+const sendEmailNodeMailer = async (receiverEmail, fileID, senderName = "Encrypt Share") => {
+  
+  console.log("receiverEmail", receiverEmail);
+
+  let config = {
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  }
+  
+  const transporter = nodemailer.createTransport(config);
+
+  const message = {
+    from: process.env.EMAIL,
+    to: receiverEmail,
+    subject: "Here is your File ID!",
+    text: `Dear user, here is your File ID: ${fileID}`,
+    html: `
+    <h3>Dear user,</h3>
+    <br/>
+    Download page: <a href='http://localhost:5173/download'>download page link</a>
+    <br />
+    Here is your File ID: <strong>${fileID}</strong>
+    <br /><br />
+    <b>Because of our security policy we don't share passwords. You need to ask the sender for it.</b>
+    `
+  }
+
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.log("Error occurred. " + err.message);
+      return err;
+    }
+    console.log("Message sent: %s", info.messageId);
+    return info;
+  }
+  );
+  };
+
+const sendEmailMailjet = async (receiverEmail, fileID, senderName = "Encrypt Share") => {
+    const mailjet = Mailjet.apiConnect(
+      process.env.MJ_APIKEY_PUBLIC,
+      process.env.MJ_APIKEY_PRIVATE
+    );
+  
+    try {
+      const request = await mailjet
+        .post('send', { version: 'v3.1' })
+        .request({
+          Messages: [
+            {
+              From: {
+                Email: "aleksandarkuzmanovic02@gmail.com",
+                Name: senderName
+              },
+              To: [
+                {
+                  Email: receiverEmail,
+                  Name: "Recipient"
+                }
+              ],
+              Subject: "Here is your File ID!",
+              TextPart: `Dear user, here is your File ID: ${fileID}`,
+              HTMLPart: `<h3>Dear user,</h3><br/> Download page: <a href='http://localhost:5173/download'>download page link</a> <br />Here is your File ID: <strong>${fileID}</strong><br /><br /><b>Because of our security policy we don't share passwords. You need to ask the sender for it.</b>`
+            }
+          ]
+        });
+  
+      if (request && request.body) {
+
+        return { success: true, data: request.body };
+      } else {
+
+        return { success: false, error: "Email sending failed" };
+      }
+    } catch (err) {
+
+      return { success: false, error: err.message };
+    }
+  };
+
+module.exports = sendEmailNodeMailer;
