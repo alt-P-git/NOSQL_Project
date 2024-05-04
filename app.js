d26a602c339e1a62306106ab1485eb1f8aaff111
theDRB123 <dhrbhanushali123@gmail.com>
2024-05-05 03:48:26 +0530

    allowed CORS

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(xss());
 
+
+app.use(cors());
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
+
 const sendEmailNodeMailer = require("./controllers/sendEmail");
 
 const limiter = rateLimit({
