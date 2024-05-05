381b2679b5861c2b74ff5182773d7bd359d94a0c
theDRB123 <dhrbhanushali123@gmail.com>
2024-05-05 09:11:20 +0530

    minor fix

--- index.js
+++ index.js
@@ -41,14 +41,14 @@ app.use(xss());
 app.enable('trust proxy')
 
 app.use(cors());
-// app.options('*', cors());
-// var allowCrossDomain = function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// }
-// app.use(allowCrossDomain);
+app.options('*', cors());
+var allowCrossDomain = function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+app.use(allowCrossDomain);
 
 const sendEmailNodeMailer = require("./controllers/sendEmail");
 
